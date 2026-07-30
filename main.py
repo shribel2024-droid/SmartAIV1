27c377d41f8adf09508f9e37323bbbc657742ff0
shribel2024-droid <shribel2024@gmail.com>
2026-07-30 22:46:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class SmartAI(App):
+    def build(self):
+        return Label(
+            text="SMARTAI PRO\nVersion 1.0",
+            font_size=24
+        )
+
+SmartAI().run()
